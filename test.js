ed26c91f40a035647d21bc3ed669fb558e377783
Lyzi Diamond <lyzidiamond@gmail.com>
2015-01-12 15:07:34 -0800

    Add test for complete LinearRings in coordinate input

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var test = require('tape');
 var polygon = require('./');
 
 test('polygon', function(t){
-  t.plan(6);
+  t.plan(7);
 
   var poly = polygon([[[5, 10], [20, 40], [40, 0]]], {name: 'test polygon'});
 
@@ -12,4 +12,5 @@ test('polygon', function(t){
   t.equal(poly.geometry.coordinates[0][2][0], 40);
   t.equal(poly.properties.name, 'test polygon');
   t.equal(poly.geometry.type, 'Polygon');
+  t.equal(polygon([[[20.0,0.0],[101.0,0.0],[101.0,1.0],[100.0,1.0],[100.0,0.0]]]).message, 'First and last coordinate pair are not equivalent', 'returns an error message');
 });
