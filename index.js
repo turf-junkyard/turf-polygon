fb33cf2e0bed9a0ffc91bed5cec019f0a61efac6
Tom MacWright <tom@macwright.org>
2015-01-14 10:51:24 -0500

    Throw error, throws another error if coordinates are wrong. Fixes #1

--- index.js
+++ index.js
@@ -6,6 +6,9 @@
  * @param {number[][]} rings - an array of LinearRings
  * @param {Object} properties - an optional properties object
  * @return {GeoJSONPolygon} output
+ * @throws {Error} throw an error if a LinearRing of the polygon has too few positions
+ * or if a LinearRing of the Polygon does not have matching Positions at the
+ * beginning & end.
  * @example
  * var poly1 = turf.polygon([[[20.0,0.0],[101.0,0.0],[101.0,1.0],[100.0,1.0],[100.0,0.0]]])
  * var poly2 = turf.polygon([[[20.0,0.0],[101.0,0.0],[101.0,1.0],[100.0,1.0],[100.0,0.0]]],
@@ -14,17 +17,22 @@
  * console.log(poly2)
  */
 module.exports = function(coordinates, properties){
-  if(coordinates === null) return new Error('No coordinates passed');
+
+  if (coordinates === null) throw new Error('No coordinates passed');
 
   for (var i = 0; i < coordinates.length; i++) {
     var ring = coordinates[i];
-
     for (var j = 0; j < ring[ring.length - 1].length; j++) {
-      if (ring[ring.length - 1][j] !== ring[0][j]) return new Error('First and last coordinate pair are not equivalent')
+      if (ring.length < 4) {
+        throw new Error('Each LinearRing of a Polygon must have 4 or more Positions.');
+      }
+      if (ring[ring.length - 1][j] !== ring[0][j]) {
+        throw new Error('First and last Position are not equivalent.');
+      }
     }
-  };
+  }
 
-  var polygon = { 
+  var polygon = {
     "type": "Feature",
     "geometry": {
       "type": "Polygon",
@@ -33,9 +41,9 @@ module.exports = function(coordinates, properties){
     "properties": properties
   };
 
-  if(!polygon.properties){
+  if (!polygon.properties) {
     polygon.properties = {};
   }
-  
+
   return polygon;
-}
+};
